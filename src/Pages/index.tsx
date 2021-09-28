1d94d8ea63e05934ee1fa3cea672cf182a6d12b7
Renan Veronez Drechsler <renanvz@outlook.com>
2021-09-28 10:10:47 -0300

    Carousel Banner

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,8 +1,10 @@
-import FirstBlockGeneral from "../Components/SectionStructure/General/blockOne";
+import { Banner } from "../Components/SectionStructure/General/Carousel";
+import FirstBlockGeneral from "../Components/SectionStructure/General/BlockOne";
 
 export function Home() {
   return (
     <>
+        <Banner />
         <FirstBlockGeneral />
     </>
   );
